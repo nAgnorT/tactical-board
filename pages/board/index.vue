1eff08b5b21fd31e19fe068a8e811ab616e5ca77
nAgnorT <trongan765@gmail.com>
2024-03-11 09:19:14 +0700

    Init Commit

---
+++ pages/board/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <svg width="1437" height="935" xmlns="http://www.w3.org/2000/svg"></svg>
+    </div>
+</template>
+<script setup>
+    definePageMeta({
+        layout: 'board'
+    })
+</script>
